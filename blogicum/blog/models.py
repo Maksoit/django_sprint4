9868ffbfe66b1058e6f2e6fdd5afb108153b986f
Vorogushin Maksim <maksim200425@yandex.ru>
2024-12-24 16:57:51 +0300

    edit PEP8

--- blogicum/blog/models.py
+++ blogicum/blog/models.py
@@ -24,6 +24,7 @@ class BaseModel(models.Model):
     def __str__(self):
         return (f'{self.is_published=}, {self.created_at=}')
 
+
 class Category(BaseModel):
     title = models.CharField(max_length=256, verbose_name='Заголовок')
     description = models.TextField(verbose_name='Описание')
@@ -97,7 +98,7 @@ class Post(BaseModel):
 
     def __str__(self):
         return self.title
-    
+
     def get_absolute_url(self):
         return reverse(
             'blog:post_detail', args=[self.pk]
